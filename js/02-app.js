edc972c522e0c2042ef0bf0632c16ad7bc1b3de7
Luis Manuel Vargas Guzman <luisvg18@gmail.com>
2023-09-19 13:54:42 -0600

    Práctica FetchAPI Finalizada!

---
+++ js/02-app.js
@@ -0,0 +1,25 @@
+const cargarJSONBtn = document.querySelector('#cargarJSON');
+cargarJSONBtn.addEventListener('click', obtenerDatos);
+
+function obtenerDatos () {
+    url = 'data/empleado.json';
+
+    fetch(url)
+        .then(respuesta => {
+            return respuesta.json();
+        })
+        .then(resultado => {
+            mostrarHTML(resultado);
+        })
+}
+
+function mostrarHTML ({empresa, id, nombre, trabajo}) {
+    const contenido = document.querySelector('.contenido');
+
+    contenido.innerHTML = `
+        <p>Empleado: ${nombre}</p>
+        <p>ID: ${id}</p>
+        <p>Empresa: ${empresa}</p>
+        <p>Trabajo: ${trabajo}</p>
+    `;
+}
